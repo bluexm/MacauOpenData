f37f65f9070e2fb2c4a1ba676d54aaefa05739b7
xavier <xavier.mathieu.office@gmail.com>
2021-07-02 20:40:10 +0800

    graph data

--- scraper/DownloadDataSets.py
+++ scraper/DownloadDataSets.py
@@ -5,6 +5,9 @@ import json
 import re 
 
 def download(conn,headers,strext):
+  """
+  Builds GET request with specific header (containing for example API key and value) 
+  """
     payload = ''
     conn.request("GET", "/"+strext, payload, headers)    
     res = conn.getresponse()
@@ -12,7 +15,9 @@ def download(conn,headers,strext):
     return data.decode("utf-8")
 
 def test_download():
-  ##strtmp = 'htt ps://dsat.apigateway.data.gov.mo/car_park_detail'
+  """
+  unit test for download function
+  """
   conn = http.client.HTTPSConnection("dst.apigateway.data.gov.mo")
   payload = ''
   headers = {
@@ -25,6 +30,11 @@ def test_download():
   print(download(conn,headers,"dst_bars"))
 
 
+
+
+"""
+Script for download of all data sets attainable through API 
+"""
 chdir('scraper')
 dfmeta=pd.read_csv('dataframe_translation.csv',header = 0)
 
@@ -32,15 +42,13 @@ for i,row in dfmeta.iterrows():
 
     #row = dfmeta.iloc[1]
     if row['API url']!=row['API url']: #nan in API address
-        #TBD : use the link on down arrow button ...  
+        #TBD : use the link on down arrow button ...  not the API 
         pass 
     else:
       fname= './data/' + re.sub("/","_",row['dataset_name_en'])+"."+row['Data Format']
-      ##strext = re.sub('data.gov.mo/\w+','\1',row['API url'])
-      ##url = re.sub('https://\w+/'+strext,'\1',row['API url'])
       strtmp = row['API url'].split('data.gov.mo/')
       strext=strtmp[-1]
-      url = row['API url'].split('/')[2] ##re.sub("/"+ strext,"",row['API url']
+      url = row['API url'].split('/')[2]
       conn = http.client.HTTPSConnection(url)
       payload = ''
       headers = { row['api_key']:row['api_val']    }
@@ -51,3 +59,5 @@ for i,row in dfmeta.iterrows():
           obj.write(data)
           obj.close
 
+#write graph of nodes 
+#TBD
---
+++ scraper/graphConnections.py
@@ -0,0 +1,48 @@
+import http.client
+from os import RTLD_NOW, chdir
+import pandas as pd 
+import json 
+import re 
+from csv import QUOTE_NONNUMERIC
+
+"""
+Script to build a graph where nodes are datasets connected if they have one field name in common.
+The fields of each dataset are read from the file dataframe_translation.csv
+The results are saved in 2 files graphEdges.csv and graphNodes.csv to be used in Gephi 
+"""
+
+chdir('scraper')
+dfmeta=pd.read_csv('dataframe_translation.csv',header = 0)
+fname_edges= '../analysis/graphEdges.csv'
+fname_nodes= '../analysis/graphNodes.csv'
+dfedges = pd.DataFrame(columns= ['Source','Target','Type','Id','Label',	'timeset','Weight'])
+dfnodes = pd.DataFrame(columns= ['Id','Label'])
+lnodes = []
+
+lignore=["[]","['']","[' ,']"]
+idxnode=0
+for i,row in dfmeta.iterrows(): # store datasets columns in a list 
+    tmpdesc = row['Field Description (English)']
+    if tmpdesc==tmpdesc: #not nan
+        #print("original",tmpdesc)
+        tmpdesc= re.sub("'-*'(,\s'-*')*","",tmpdesc)
+        if tmpdesc not in lignore:
+            #print(tmpdesc)
+            lnodes.append({'name':row['dataset_name_en'], 'id':i, 'cols':set(tmpdesc[2:-2].split("', '"))})
+            dfnodes = dfnodes.append({'Id':idxnode,'Label':row['dataset_name_en']}, ignore_index=True)
+            idxnode+=1
+dfnodes.to_csv(fname_nodes,index=False, quotechar='"', quoting=QUOTE_NONNUMERIC, mode='a') 
+
+for i in range(len(lnodes)-1): # look at common nodes (columns names)
+    print('--------->>> checking <<<<<------------ ', lnodes[i]['cols'])
+    for j in range(i+1,len(lnodes)): 
+        #print(lnodes[j]['cols'])
+        cfields =lnodes[i]['cols'].intersection(lnodes[j]['cols']) #common fields
+        w = len(cfields)
+        if w>0:
+            #print("{} edges".format(w))
+            #print(lnodes[j]['cols'])
+            dfedges = dfedges.append({'Source':lnodes[i]['id'],'Target':lnodes[j]['id'],'Type':'Directed','Id':len(dfedges)+1,'Label':lnodes[i]['name']+lnodes[j]['name'],'timeset':'','Weight':1}, ignore_index=True)
+dfedges.to_csv(fname_edges,index=False, quotechar='"', quoting=QUOTE_NONNUMERIC) 
+
+print("finished, found {} nodes and {} edges".format(len(dfnodes),len(dfedges)))
